7d8f745aad62696e58e2dd017cd3b8717db7c701
samee1528 <dineshkosala1528@gmail.com>
2022-07-12 14:35:17 +0530

    Add Files

---
+++ src/Appinitializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/StudentForm.fxml"))));
+        stage.show();
+    }
+}
